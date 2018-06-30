00bf234f413050d206a4d1cc84f30720232e0dff
zhujian1989 <370159662@qq.com>
2018-06-30 11:16:10 +0800

    commit

--- lib/widget/demo_advance/inherited_w_test_b.dart
+++ lib/widget/demo_advance/inherited_w_test_b.dart
@@ -12,10 +12,7 @@ class TestWidgetB extends StatelessWidget {
 
     return  new Padding(
       padding: const EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0),
-      child: new RaisedButton(
-          textColor: Colors.black,
-          child: new Text('${inheritedTestModel.count}'),
-          onPressed: inheritedContext.increment),
+      child:  new Text('当前count:${inheritedTestModel.count}',style: new TextStyle(fontSize: 20.0),),
     );
   }
 }
