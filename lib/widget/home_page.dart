ff452ad93d50af6412dae3198559f87dd6a1ca20
zhujian1989 <370159662@qq.com>
2018-07-16 17:18:08 +0800

    commit

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_study/widget/demo_localizations/localizations_study.dart';
+import 'package:flutter_study/widget/drawer_page.dart';
 import 'package:flutter_study/widget/tab_android_page.dart';
 import 'package:flutter_study/widget/tab_girl_page.dart';
 import 'package:flutter_study/widget/tab_ios_page.dart';
-import 'package:flutter_study/widget/drawer_page.dart';
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => new _HomePageState();
@@ -13,6 +16,7 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
 
   final _key = new GlobalKey<ScaffoldState>();
+
   @override
   void initState() {
     super.initState();
@@ -38,10 +42,30 @@ class _HomePageState extends State<HomePage>
     );
   }
 
+  _showDialog() {
+    showDialog<Null>(
+      context: context,
+      child: new AlertDialog(content: new Text('退出app'), actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+                Navigator.pop(context);
+            },
+            child: new Text('确定'))
+      ]),
+    );
+  }
+
+
+  Future<bool> _requestPop() {
+    _showDialog();
+    return new Future.value(false);
+  }
+
   @override
   Widget build(BuildContext context) {
     _initPage();
-    return new Scaffold(
+    return new WillPopScope(child: new Scaffold(
       key: _key,
       appBar: new AppBar(
         title: new Text(StudyLocalizations.of(context)
@@ -69,6 +93,6 @@ class _HomePageState extends State<HomePage>
         },
       ),
       body: _body,
-    );
+    ), onWillPop: _requestPop);
   }
 }
